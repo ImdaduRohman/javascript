a1e48b9c89ee0bfbad2e7de06c52357e55e13905
ImdaduRohman <imdadu.ylabs1@gmail.com>
2023-03-12 06:44:39 +0700

    reduce

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-//filter
+//VARIABLE
 const angka = [2, 5, 2, 4, 7, 4, 9, 8, 9, 6];
-const filterAngka = angka.filter(a => a > 5);
-
 const todos = [
     {
         id: 1,
@@ -15,6 +13,16 @@ const todos = [
         id: 3,
         todo: "mandi"
     }
-]
+];
+
+
+//filter
+const filterAngka = angka.filter(a => a > 5);
 const filterTodo = todos.filter(a => a.todo === "belajar");
-console.log(filterTodo[0].todo, "<== filter todo")
+
+
+//map
+const mapAngka = angka.map(a => a*2);
+
+//reduce 
+const readuceAngka = angka.reduce((a, b ) => a + b)
